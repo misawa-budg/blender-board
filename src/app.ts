514a0560a95c3ea25327e03c70ee73c3e51467c4
misawa-budg <hmisawa426@keio.jp>
2026-02-25 09:53:56 +0900

    route(images): /imagesのルートファイルを追加

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import rootRouter from "./routes/root.js";
 import modelsRouter from "./routes/models.js";
+import imagesRouter from "./routes/images.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/", rootRouter);
 app.use("/models", modelsRouter);
+app.use("/images", imagesRouter);
 
 export default app;
