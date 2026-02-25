040b2b5cd39c2db7fe57120bd3e7b31eab7daab0
misawa-budg <hmisawa426@keio.jp>
2026-02-25 09:52:11 +0900

    route(models): /modelsのルートファイルを追加

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import rootRouter from "./routes/root.js";
+import modelsRouter from "./routes/models.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/", rootRouter);
+app.use("/models", modelsRouter);
 
 export default app;
